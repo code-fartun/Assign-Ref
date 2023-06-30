35c4dd7385894b416f3c2b6a294b1e84d1eb276e
Fartun Jama <62263190+code-fartun@users.noreply.github.com>
2023-06-30 09:05:53 -0700

    Update SQLFriends_SelectAll.sql

--- SQL/SQLFriends_SelectAll.sql
+++ SQL/SQLFriends_SelectAll.sql
@@ -1,40 +1,32 @@
-﻿
-ALTER proc [dbo].[Friends_SelectAllV3]
-		--@Id int
+﻿ALTER proc [dbo].[Friends_SelectAllV3]
+		
 as
 /*
 	
 
-	Execute dbo.Friends_SelectAllV3
+Execute dbo.Friends_SelectAllV3
 
 */
 
 BEGIN
 
-  SELECT	f.[Id]
-		,f.[Title]
-		,f.[Bio]
-     		,f.[Summary]
-	 	,f.[Headline]
-	      ,f.[Slug]
-	      ,f.[StatusId]
-		  ,i.[Id]   --as ImageId
-		  ,i.[Url]
-		   ,i.[TypeId]
-		  --,i.[Url]
-		   ,f.[UserId]
-	      ,f.[DateCreated]
-	      ,f.[DateModified]
+SELECT	f.[Id]
+	,f.[Title]
+	,f.[Bio]
+     	,f.[Summary]
+	 ,f.[Headline]
+	 ,f.[Slug]
+	 ,f.[StatusId]
+	 ,i.[Id]  
+	 ,i.[Url]
+	 ,i.[TypeId]
+	 ,f.[UserId]
+	 ,f.[DateCreated]
+	 ,f.[DateModified]
      
 
      
-
-  FROM [dbo].[FriendsV2] as f inner join dbo.Images as i
-  ON f.PrimaryImageId = i.Id
- -- Where f.Id = @Id
-
+FROM [dbo].[FriendsV2] as f inner join dbo.Images as i
+ON f.PrimaryImageId = i.Id
 
 END
-
-
-
