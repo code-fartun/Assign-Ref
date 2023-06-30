40502edd6ed48373aa8ab157b75e50047ac67b60
Fartun Jama <62263190+code-fartun@users.noreply.github.com>
2023-06-30 09:00:19 -0700

    Update SQLFriend_Pagination.sql

--- SQL/SQLFriend_Pagination.sql
+++ SQL/SQLFriend_Pagination.sql
@@ -1,42 +1,36 @@
 ﻿ALTER proc [dbo].[Friends_Pagination]
-			@PageIndex int
-			,@PageSize int
-		
+			         @PageIndex int
+			         ,@PageSize int
+		 
 
 as 
 /*
 Declare
-
-Execute dbo.Friends_Pagination  @PageIndex
-				,@PageSize
-
-	Select * 
-	from dbo.Friends
+Execute dbo.Friends_Pagination   @PageIndex
+				 ,@PageSize
 
 */
 BEGIN
+Declare  @offset int=@PageIndex *@PageSize
 
-Declare @offset int=@PageIndex *@PageSize
-
-SELECT [Id]
-      ,[Title]
-      ,[Bio]
-      ,[Summary]
-      ,[Headline]
-      ,[Slug]
-      ,[StatusId]
-      ,[PrimaryImageUrl]
-      ,[DateCreated]
-      ,[DateModified]
-      ,[UserId]
-	,[TotalCount]=COUNT (1) OVER()
-
-  FROM [dbo].[Friends]
-  ORDER BY Id
-  OFFSET @offset Rows
-  Fetch Next @PageSize Rows ONLY
-
-  END
+SELECT     			  [Id]
+      				 ,[Title]
+    				 ,[Bio]
+     				 ,[Summary]
+      				 ,[Headline]
+     				 ,[Slug]
+     				 ,[StatusId]
+     				 ,[PrimaryImageUrl]
+    				 ,[DateCreated]
+     				 ,[DateModified]
+      				 ,[UserId]
+				 ,[TotalCount]=COUNT (1) OVER()
+FROM [dbo].[Friends]
+ORDER BY Id
+OFFSET @offset Rows
+Fetch Next @PageSize Rows ONLY
+	
+END
 
 
 
